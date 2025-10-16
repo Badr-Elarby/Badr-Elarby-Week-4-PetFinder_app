f3197c04314cb9d402e7f537c513756869098458
Badr-Elarby <badrshahd291@gmail.com>
2025-10-16 16:40:53 +0300

    feat(splash): implement splash screen and initialize core setup
    - Added Splash Screen UI
    - Created core folder structure (di, network, utils, etc.)
    - Added AppColors and AppStyles files with base theme setup
    - Installed and configured required dependencies in pubspec.yaml
    - Prepared base project setup for upcoming features

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color HeadingBlack = Color(0xFF000000);
+  static const Color SubTextGrey = Color(0xFF646464);
+  static const Color MintGreen = Color(0xFF44BDB6);
+  static const Color CoralRed = Color(0xFFEF594A);
+  static const Color LightBackground = Color(0xFFE1F8F9);
+}
